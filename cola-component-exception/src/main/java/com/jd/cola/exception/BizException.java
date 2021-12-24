f1bb866fc451cec7563f6cce13ef9f9de638bd3a
zhanglu312 <zhanglu312@jd.com>
2021-12-24 09:52:06 +0800

    添加异常组件

---
+++ cola-component-exception/src/main/java/com/jd/cola/exception/BizException.java
@@ -0,0 +1,29 @@
+package com.jd.cola.exception;
+
+/**
+ * Business Exception is known Exception, no need retry
+ *
+ * @author deer
+ * @date 2021-12-23
+ */
+public class BizException extends BaseException {
+    private static final long serialVersionUID = 1L;
+
+    private static final String DEFAULT_ERR_CODE = "BIZ_ERROR";
+
+    public BizException(String errMessage) {
+        super(DEFAULT_ERR_CODE, errMessage);
+    }
+
+    public BizException(String errCode, String errMessage) {
+        super(errCode, errMessage);
+    }
+
+    public BizException(String errMessage, Throwable e) {
+        super(DEFAULT_ERR_CODE, errMessage, e);
+    }
+
+    public BizException(String errorCode, String errMessage, Throwable e) {
+        super(errorCode, errMessage, e);
+    }
+}
---
+++ cola-component-exception/src/main/java/com/jd/cola/exception/SysException.java
@@ -0,0 +1,29 @@
+package com.jd.cola.exception;
+
+/**
+ * System Exception is unexpected Exception, retry might work again
+ *
+ * @author deer
+ * @date 2021-12-23
+ */
+public class SysException extends BaseException {
+    private static final long serialVersionUID = 1L;
+
+    private static final String DEFAULT_ERR_CODE = "SYS_ERROR";
+
+    public SysException(String errMessage) {
+        super(DEFAULT_ERR_CODE, errMessage);
+    }
+
+    public SysException(String errCode, String errMessage) {
+        super(errCode, errMessage);
+    }
+
+    public SysException(String errMessage, Throwable e) {
+        super(DEFAULT_ERR_CODE, errMessage, e);
+    }
+
+    public SysException(String errorCode, String errMessage, Throwable e) {
+        super(errorCode, errMessage, e);
+    }
+}
